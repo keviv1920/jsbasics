f23fd87ef45cb9d6681e9b28b81ba5bd23aab5e5
Vivek Ruhela <vivekruhela1920@gmail.com>
2021-09-02 19:30:17 +0530

    console

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('Hey this is my first piece of JS code!!! YAY')
+
+console.log(12490239043902)
+
+console.log({ name: "Yutika" })
+
+console.log(true)
+console.log(false)
+
+console.log(false, 234243, 'string', { name: "Alfred" })
+
+console.warn("This is a warning")
+
+console.error("this is an error")
